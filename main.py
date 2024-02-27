cbf0302cd9157e3d63288a17d80c1c71b2a9a328
iamtonmoy0 <tonmoy2code@gmail.com>
2024-02-28 03:23:50 +0600

    model added and get all and create todo added

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
-
+from model import Todo
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+todos=[]
+# get all todo
+@app.get('/todos')
+async def get_todos():
+    return { 'todo': todos }
+# get single todo
+# create new todo
+@app.post('/todos')
+async def create_todos(todo:Todo):
+    todos.append(todo)
+    return { 'message': "new todo added" }
+# update existing todo
+# delete a todo
