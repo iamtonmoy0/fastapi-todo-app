93b6e308da7316272b5acf878da41ecfa5ec16d5
iamtonmoy0 <tonmoy2code@gmail.com>
2024-02-28 03:39:35 +0600

    delete todo added

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def get_todos():
     return { 'todo': todos }
 # get single todo
 @app.get('/todos/{item_id}')
-async def get_todos(item_id:int):
+async def get_single_todos(item_id:int):
     for todo in todos:
         if todo.id==item_id:
             return {"todo",todo}
@@ -25,4 +25,12 @@ async def create_todos(todo:Todo):
     todos.append(todo)
     return { 'message': "new todo added" }
 # update existing todo
+
 # delete a todo
+@app.delete('/todos/{item_id}')
+async def delete_todos(item_id:int):
+    for todo in todos:
+        if todo.id==item_id:
+            todos.remove(todo)
+            
+	return {"message":"no todo found"}
